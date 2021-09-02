98eb268b35cf6083dab80f2618179c00343dd25c
Meshari <alh3rbi@gmail.com>
2021-09-01 21:29:27 -0600

    bug disabled redux devtools

--- src/state/store.js
+++ src/state/store.js
@@ -3,11 +3,5 @@ import reducers from "./reducers";
 import thunk from "redux-thunk";
 
 const middleware = [thunk];
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducers, compose(applyMiddleware(...middleware)));
 export default store;
